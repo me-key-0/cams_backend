d902f695dc6b290c724a674b2d3361ca47caa6f7
me-key-0 <me.key.as.24@gmail.com>
2025-05-19 19:14:12 -0400

    grade service updated

--- course-service/src/main/java/com/cams/course_service/controller/EnrollmentController.java
+++ course-service/src/main/java/com/cams/course_service/controller/EnrollmentController.java
@@ -3,6 +3,8 @@ package com.cams.course_service.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.cams.course_service.dto.CourseSessionDto;
@@ -24,22 +26,22 @@ public class EnrollmentController {
         return enrollmentService.getCourseSessions(studentId, year, semester, academicYear);
     }
 
-    // @GetMapping("/student/{studentId}")
-    // public ResponseEntity<List<CourseSessionDto>> getCourseSessionByStudentId(@PathVariable Long studentId) {
-    //     try {
-    //         System.out.println("Received request for student ID: " + studentId);
-    //         List<CourseSessionDto> dto = enrollmentService.getCourseSessionsByStudent(studentId);
-    //         System.out.println("Found " + (dto != null ? dto.size() : "null") + " course sessions");
+    @GetMapping("/student/{studentId}")
+    public ResponseEntity<List<CourseSessionDto>> getCourseSessionByStudentId(@PathVariable Long studentId) {
+        try {
+            System.out.println("Received request for student ID: " + studentId);
+            List<CourseSessionDto> dto = enrollmentService.getCourseSessionsByStudent(studentId);
+            System.out.println("Found " + (dto != null ? dto.size() : "null") + " course sessions");
             
-    //         if (dto == null || dto.isEmpty()) {
-    //             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    //         }
+            if (dto == null || dto.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
             
-    //         return new ResponseEntity<>(dto, HttpStatus.OK);
-    //     } catch (Exception e) {
-    //         System.err.println("Error processing request: " + e.getMessage());
-    //         e.printStackTrace();
-    //         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    //     }
-    // }
+            return new ResponseEntity<List<CourseSessionDto>>(dto, HttpStatus.OK);
+        } catch (Exception e) {
+            System.err.println("Error processing request: " + e.getMessage());
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
